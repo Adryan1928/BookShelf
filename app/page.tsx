bd2765874e47c0f7fe989c0c5cc341bf9b1ad2f0
yasmingaspar <yasminscgaspar@hotmail.com>
2025-09-30 01:59:51 -0300

    Implementa Dashboard com Shadcn/ui e Sistema de Temas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { StatCard } from "@/components/statCard";
-import { BookCard } from "@/components/bookCard";
 import { Book, BookOpenCheck, CheckCircle, FileText } from "lucide-react";
-
+import { BookCard } from "@/components/bookCard";
+import { StatCard } from "@/components/statCard";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const stats = [
   { title: "Total de Livros", value: 120, icon: <Book size={32} /> },
@@ -14,25 +14,21 @@ const recentlyReadBooks = [
   {
     title: "O Quinze",
     author: "Rachel de Queiroz",
-
     coverUrl: "https://i.pinimg.com/736x/ee/ea/36/eeea361df319626c4e385196b3a14116.jpg",
   },
   {
     title: "A Revolução dos Bichos",
     author: "George Orwell",
-    
-    coverUrl: "https://s3.static.brasilescola.uol.com.br/be/2025/03/capa-do-livro-a-revolucao-dos-bichos-de-george-orwell-publicado-pela-editora-principis.jpg",
+    coverUrl: "https://i.pinimg.com/1200x/e8/31/ef/e831efc6b89b6c4af9e7fd7457370319.jpg",
   },
   {
     title: "O Hobbit",
     author: "J.R.R. Tolkien",
-    
     coverUrl: "https://i.pinimg.com/736x/4b/09/98/4b09981418a482010e228d8dd7bad1d9.jpg",
   },
   {
     title: "Fahrenheit 451",
     author: "Ray Bradbury",
-
     coverUrl: "https://i.pinimg.com/736x/7c/5b/15/7c5b153fdc40f631c995f55d26026346.jpg",
   },
 ];
@@ -40,7 +36,13 @@ const recentlyReadBooks = [
 export default function DashboardPage() {
   return (
     <div className="space-y-8">
-      {/* Seção de Estatísticas */}
+      <header className="flex justify-between items-center">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <div className="flex items-center gap-4">
+          <ThemeToggle />
+        </div>
+      </header>
+      
       <section>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {stats.map((stat) => (
@@ -54,13 +56,12 @@ export default function DashboardPage() {
         </div>
       </section>
 
-      {/* Seção de Lidos Recentemente */}
       <section>
-        <div className="bg-white p-8 rounded-lg shadow-sm">
-          <h2 className="text-xl font-bold text-gray-800 mb-4">
+        <div className="bg-card p-8 rounded-lg">
+          <h2 className="text-xl font-bold mb-6">
             Lidos Recentemente
           </h2>
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
+          <div className="grid grid-cols-2 sm-grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8">
             {recentlyReadBooks.map((book) => (
               <BookCard
                 key={book.title}
