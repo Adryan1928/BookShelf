04c90d8545df99e5b5328c2a2e7b790991de72db
Adryan1928 <adryanoliveira1928@gmail.com>
2025-10-06 14:46:31 -0300

    front de editar

--- app/edit-book/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoadingEditBook() {
-  return (
-    <div className="flex items-center justify-center h-64">
-      <p className="text-gray-600 animate-pulse">
-        ⏳ Carregando formulário de edição do livro...
-      </p>
-    </div>
-  );
-}
